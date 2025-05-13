04fe08e3347f4a925d751634c618039ecb364c8f
Anastasia Samsonadze <145012482+AnastasiaSamsonadze@users.noreply.github.com>
2025-05-13 19:43:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿// ANASTASIA SAMSONADZE WWV6IR
+
+
+using System;
+
+struct FuelData
+{
+    public string Date;
+    public double Amount;
+    public int Quantities;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const int MaxN = 100;
+
+        FuelData[] fuelData = new FuelData[MaxN];
+
+        // Input data
+        int N = int.Parse(Console.ReadLine()!);
+        for (int i = 0; i < N; i++)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            fuelData[i] = new FuelData
+            {
+                Date = input[0],
+                Amount = double.Parse(input[1]),
+                Quantities = int.Parse(input[2])
+            };
+        }
+
+        // Subtask A
+        bool exist = false;
+        string when = "never";
+        for (int i = 0; i < N; i++)
+        {
+            if (fuelData[i].Amount == 20000)
+            {
+                exist = true;
+                when = fuelData[i].Date;
+                break;
+            }
+        }
+        Console.WriteLine($"Subtask A: {when}");
+
+        // Subtask B
+        double sum = 0;
+        for (int i = 0; i < N; i++)
+        {
+            sum += fuelData[i].Amount;
+        }
+        Console.WriteLine($"Subtask B: {sum}");
+
+        // Subtask C
+        int cnt = 0;
+        bool[] days = new bool[MaxN];
+        for (int i = 0; i < N; i++)
+        {
+            if (fuelData[i].Quantities > 40)
+            {
+                days[i] = true;
+                cnt++;
+            }
+        }
+        Console.Write("Subtask C: ");
+        if (cnt == 0)
+        {
+            Console.WriteLine("none");
+        }
+        else
+        {
+            for (int i = 0; i < N; i++)
+            {
+                if (days[i])
+                {
+                    Console.Write(fuelData[i].Date + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
---
+++ b3/b3/Program.cs
@@ -0,0 +1,64 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        string[] names = new string[N];
+        int[] populations = new int[N];
+        int[] participants = new int[N];
+        for (int i = 0; i < N; i++)
+        {
+            names[i] = Console.ReadLine();
+            populations[i] = Convert.ToInt32(Console.ReadLine());
+            participants[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        int[] categories = new int[7];
+        for (int i = 0; i < N; i++)
+        {
+            if (populations[i] < 700)
+            {
+                categories[0] += participants[i];
+            }
+            else if (populations[i] >= 700 && populations[i] < 1500)
+            {
+                categories[1] += participants[i];
+            }
+            else if (populations[i] >= 1500 && populations[i] < 3000)
+            {
+                categories[2] += participants[i];
+            }
+            else if (populations[i] >= 3000 && populations[i] < 8000)
+            {
+                categories[3] += participants[i];
+            }
+            else if (populations[i] >= 8000 && populations[i] < 25000)
+            {
+                categories[4] += participants[i];
+            }
+            else if (populations[i] >= 25000 && populations[i] < 70000)
+            {
+                categories[5] += participants[i];
+            }
+            else if (populations[i] >= 70000)
+            {
+                categories[6] += participants[i];
+            }
+        }
+        for (int j = 0; j < 7; j++)
+        {
+            Console.Write(categories[j] + " ");
+        }
+        int max = 0;
+        int index = 0;
+        for (int i = 0; i < N; i++)
+        {
+            if (participants[i] > max)
+            {
+                max = participants[i];
+                index = i;
+            }
+        }
+        Console.WriteLine("\n" + names[index]);
+        Console.ReadLine();
+    }
+}
